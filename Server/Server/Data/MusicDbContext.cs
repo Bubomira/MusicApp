3225a63c1773ce8d61fe5be751d873ecad6481c7
Bubomira <106514492+Bubomira@users.noreply.github.com>
2023-05-28 14:55:38 +0300

    added dbcontext, dbsets on all entities and cpk for albumsusers

---
+++ Server/Server/Data/MusicDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using Server.Models;
+
+namespace Server.Data
+{
+    public class MusicDbContext:DbContext
+    {
+        public MusicDbContext(DbContextOptions<MusicDbContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<Performer> Performers { get; set; }
+
+        public DbSet<Album> Albums { get; set; }
+
+        public DbSet<Playlist> Playlists { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<AlbumsUsers> AlbumsUsers { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AlbumsUsers>()
+                .HasKey(pc => new { pc.UserId, pc.AlbumId });
+
+            modelBuilder.Entity<AlbumsUsers>()
+                .HasOne(au => au.User)
+                .WithMany(au => au.AlbumsUsers)
+                .HasForeignKey(au => au.UserId);
+
+            modelBuilder.Entity<AlbumsUsers>()
+                .HasOne(au => au.Album)
+                .WithMany(au => au.AlbumsUsers)
+                .HasForeignKey(au => au.AlbumId);
+
+        }
+    }
+}
