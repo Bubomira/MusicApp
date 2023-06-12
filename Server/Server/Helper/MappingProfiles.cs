817dd405c94621a9d842963ee2aa169b59ca7f60
Bubomira <106514492+Bubomira@users.noreply.github.com>
2023-06-13 00:00:04 +0300

    mapper profile, map for song added

--- Server/Server/Helper/MappingProfiles.cs
+++ Server/Server/Helper/MappingProfiles.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using Server.DTO.SongDTO;
+using Server.Models;
 
 namespace Server.Helper
 {
@@ -6,7 +8,16 @@ namespace Server.Helper
     {
         public MappingProfiles()
         {
-            
+            CreateMap<Song, SongDetailsDto>()
+                .ForMember(s => s.PerformerName,
+                   opt => opt.MapFrom(src =>
+                   src.Album.Performer.Name))
+                .ForMember(s => s.AlbumName,
+                   opt => opt.MapFrom(src =>
+                   src.Album.Name))
+                .ForMember(s => s.SecondaryPerformers,
+                     opt => opt.MapFrom(src =>
+                     src.SecondaryPerformers.Select(x => x.Performer.Name).ToList()));
         }
 
     }
