cef97521b260b41d4fb1224e852e0265d70cbc87
Bubomira <106514492+Bubomira@users.noreply.github.com>
2023-06-14 23:22:57 +0300

    new song dto

---
+++ Server/Server/DTO/SongDTO/NormalSongDto.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper.Configuration.Annotations;
+using Server.Models;
+
+namespace Server.DTO.SongDTO
+{
+    public class NormalSongDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [SourceMember(nameof(Song.Album.Performer.Name))]
+        public string PerformerName { get; set; }
+
+        public ICollection<string>? SecondaryPerformers { get; set; }
+    }
+}
