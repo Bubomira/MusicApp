593f11e62e84921fa5b324aa5020dedb0e0b4a2c
Bubomira <106514492+Bubomira@users.noreply.github.com>
2023-06-16 19:19:09 +0300

    added UserRepository
    for register, login, logout and checking if a user exists

---
+++ Server/Server/Interfaces/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using Server.Models;
+
+namespace Server.Interfaces
+{
+    public interface IUserRepository
+    {
+        public Task<User> RegisterUser();
+
+        public Task<User> LoginUser();
+
+        public Task<bool> LogoutUser();
+
+        public Task<bool> CheckIfUserExists();
+
+    }
+}
---
+++ Server/Server/Repository/UserRepository.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Server.Data;
+using Server.Interfaces;
+using Server.Models;
+
+namespace Server.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly MusicDbContext _musicDbContext;
+        public UserRepository(MusicDbContext musicDbContext)
+        {
+            _musicDbContext = musicDbContext;
+        }
+        public Task<User> LoginUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> RegisterUser()
+        {
+            throw new NotImplementedException();
+        }
+        public Task<bool> LogoutUser()
+        {
+            throw new NotImplementedException();
+        }
+        public Task<bool> CheckIfUserExists()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
