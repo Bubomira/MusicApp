489dce61cde6d2c2ab876e2d4b9e7631ffe77bc8
Bubomira <106514492+Bubomira@users.noreply.github.com>
2023-06-20 23:57:13 +0300

    .

--- Server/Server/DTO/PlaylistDto/ExportDetailedPlaylistDto.cs
+++ Server/Server/DTO/PlaylistDto/ExportDetailedPlaylistDto.cs
@@ -2,7 +2,7 @@
 
 namespace Server.DTO.PlaylistDto
 {
-    public class ExportDetailedPlaylist
+    public class ExportDetailedPlaylistDto
     {
         public int Id { get; set; }
         public string Name { get; set; }
