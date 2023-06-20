4a9b41f2a452e8a1f03a8547ae9c31c24c70e79f
Bubomira <106514492+Bubomira@users.noreply.github.com>
2023-06-20 22:59:58 +0300

    playlistDto

---
+++ Server/Server/DTO/PlaylistDto/ExportDetailedPlaylist.cs
@@ -0,0 +1,18 @@
+﻿using Server.DTO.SongDTO;
+
+namespace Server.DTO.PlaylistDto
+{
+    public class ExportDetailedPlaylist
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int Likes { get; set; }
+
+        public List<NormalSongDto> Songs { get; set; }
+
+        public int OwnerId { get; set; }
+
+        public string OwnerName { get; set; }
+    }
+}
