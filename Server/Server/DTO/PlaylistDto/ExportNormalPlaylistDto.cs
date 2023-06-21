0129fca15d5ae80af181eaabff3c15e84139221c
Bubomira <106514492+Bubomira@users.noreply.github.com>
2023-06-21 22:40:29 +0300

    new playlist dto

---
+++ Server/Server/DTO/PlaylistDto/ExportNormalPlaylistDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Server.DTO.PlaylistDto
+{
+    public class ExportNormalPlaylistDto
+    {
+        public int PlaylistId { get; set; }
+
+        public string PlaylistName { get; set; }
+
+        public int OwnerId { get; set; }
+
+        public string OwnerName { get; set; }
+    }
+}
