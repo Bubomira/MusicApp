8eb59fbc8e65def35019c293d8b8a6e20c46327e
Bubomira <106514492+Bubomira@users.noreply.github.com>
2023-07-01 13:15:24 +0300

    albumDtos

---
+++ Server/Server/DTO/AlbumDto/ExportDetailedAlbumDto.cs
@@ -0,0 +1,18 @@
+﻿using Server.DTO.SongDTO;
+
+namespace Server.DTO.AlbumDto
+{
+    public class ExportDetailedAlbumDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string PerformerName { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public string Notes { get; set; }
+
+        public List<NormalSongDto> Songs { get; set; }
+    }
+}
---
+++ Server/Server/DTO/AlbumDto/ExportNormalAlbumDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Server.DTO.AlbumDto
+{
+    public class ExportNormalAlbumDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string PerformerName { get; set; }
+
+    }
+}
