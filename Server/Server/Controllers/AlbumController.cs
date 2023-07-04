da1b08d2fa33e999484fe4ad4b02b2369207a23a
Bubomira <106514492+Bubomira@users.noreply.github.com>
2023-07-04 16:31:24 +0300

    like+dislike album

--- Server/Server/Controllers/AlbumController.cs
+++ Server/Server/Controllers/AlbumController.cs
@@ -1,27 +1,29 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Server.DTO.AlbumDto;
 using Server.Interfaces;
+using System.Security.Claims;
 
 namespace Server.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AlbumController:Controller
+    public class AlbumController : Controller
     {
         private readonly IAlbumRepository _albumRepository;
 
         private readonly IMapper _mapper;
 
-        public AlbumController(IAlbumRepository albumRepository,IMapper mapper)
+        public AlbumController(IAlbumRepository albumRepository, IMapper mapper)
         {
             _albumRepository = albumRepository;
             _mapper = mapper;
         }
-        [Route("details/{albumId}")]
+        [HttpGet("details/{albumId}")]
         public async Task<IActionResult> GetAlbumDetails(int albumId)
         {
-            if(!await _albumRepository.CheckIfAlbumExists(albumId))
+            if (!await _albumRepository.CheckIfAlbumExists(albumId))
             {
                 return NotFound();
             }
@@ -29,6 +31,47 @@ namespace Server.Controllers
             var albumDetailsDto = _mapper.Map<ExportDetailedAlbumDto>(albumDetails);
             return Ok(albumDetailsDto);
         }
+
+        [Authorize]
+        [HttpGet("like/{albumId}")]
+        public async Task<IActionResult> LikeAlbum(int albumId)
+        {
+            if (!await _albumRepository.CheckIfAlbumExists(albumId))
+            {
+                return NotFound("Album does not exist!");
+            }
+            var userId = int.Parse(User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier).Value);
+
+            if (await _albumRepository.CheckIfAlbumIsLikedByUser(userId, albumId))
+            {
+                return Forbid("User has already liked the album!");
+            }
+
+            _albumRepository.LikeAlbum(userId, albumId);
+            return NoContent();
+        }
+
+        [Authorize]
+        [HttpGet("dislike/{albumId}")]
+
+        public async Task<IActionResult> DislikeAlbum(int albumId)
+        {
+            if (!await _albumRepository.CheckIfAlbumExists(albumId))
+            {
+                return NotFound("Album does not exist!");
+            }
+            var userId = int.Parse(User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier).Value);
+
+            if (!await _albumRepository.CheckIfAlbumIsLikedByUser(userId, albumId))
+            {
+                return Forbid("User has not liked the current album!");
+            }
+
+            _albumRepository.LikeAlbum(userId, albumId);
+            return NoContent();
+        }
+
+
 
     }
 }
