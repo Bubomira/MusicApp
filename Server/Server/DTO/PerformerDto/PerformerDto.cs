ecf1fc1a814c6f517b786781305a6c70dd395c83
Bubomira <106514492+Bubomira@users.noreply.github.com>
2023-07-11 16:53:06 +0300

    performerDto

---
+++ Server/Server/DTO/PerformerDto/PerformerDto.cs
@@ -0,0 +1,15 @@
+﻿using Server.DTO.SongDTO;
+
+namespace Server.DTO.PerformerDto
+{
+    public class PerformerDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Notes { get; set; }
+
+        public List<PopularSongDto> PopularSongs { get; set; }
+    }
+}
