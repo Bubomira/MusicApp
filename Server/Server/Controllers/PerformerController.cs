8ec9314e55f1bf86c08a9d72a93ea37e87f0d85f
Bubomira <106514492+Bubomira@users.noreply.github.com>
2023-07-11 16:53:59 +0300

    get performer by id method

---
+++ Server/Server/Controllers/PerformerController.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Server.DTO.PerformerDto;
+using Server.Interfaces;
+
+namespace Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PerformerController:Controller
+    {
+        private readonly IPerformerRepository _performerRepository;
+        private readonly IMapper _mapper;
+        public PerformerController(IPerformerRepository performerRepository,IMapper mapper)
+        {
+            _performerRepository = performerRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet("performer/{performerId}")]
+        public async Task<IActionResult> GetPerformerDetails(int performerId)
+        {
+            if (! await _performerRepository.CheckIfPerformerExists(performerId))
+            {
+                return NotFound();
+            }
+            var performer = await _performerRepository.GetPerformerById(performerId);
+
+            var performerDto = _mapper.Map<PerformerDto>(performer);
+
+            return Ok(performer);
+
+        }
+    }
+}
